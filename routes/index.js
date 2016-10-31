895805c47e025322b94897a6c8d4451856c0bb0a
liuyang1520 <liuyang1520@gmail.com>
2016-10-31 15:15:47 -0600

    Add JQuery and Pure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Invmans', content: 'Welcome to Inventory Management System'});
 });
 
 module.exports = router;

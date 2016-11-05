edb8e1744fd3d5ab4e8ad0678b3cbb4f05f0c708
liuyang1520 <liuyang1520@gmail.com>
2016-11-04 19:50:56 -0600

    Add some routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../db/connection");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('inventory_list', { title: 'List '});
+	res.render('tableList', { title: 'List '});
+});
+
+router.get('/tables/:tableName', function(req, res, next) {
+	db.models[req.params['tableName']].findAll({raw: true})
+	.then(
+		function(results) {
+			res.render('tableList', { title: req.params['tableName'], content: results});
+		});
+});
+
+router.get('/tables/:tableName/:id', function(req, res, next) {
+	db.models[req.params['tableName']].findAll({raw: true})
+	.then(
+		function(results) {
+			res.render('tableList', { title: req.params['tableName'], content: results});
+		});
 });
 
 module.exports = router;
